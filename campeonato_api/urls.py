1ac9d3d9c0dcee339dc2ad056191b626949e0e1a
Ander19 <ndrsnvenega@gmail.com>
2024-03-27 13:14:49 -0500

    probando rama dev

--- campeonato_api/urls.py
+++ campeonato_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.contrib import admin
-from apps.base.views import Login,Logout,FirebaseRegisterView      
+from apps.base.views import Login,Logout,FirebaseRegisterView,LoginWhitGoogle
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -18,9 +18,9 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Documentacion API",
       default_version='v1',
-      description="Documentacion API campeonatos futbol",
+      description="Documentacion API Campeonatos de Futbol",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="ndrsnvenegas@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -36,13 +36,14 @@ urlpatterns = [
    path('campeonatos/',include('apps.campeonatos.api.urls')),
    path('encuentros/',include('apps.encuentros.api.urls')),
    path('equipos/',include('apps.equipos.api.urls')),
-   path("jugadores/", include('apps.jugadores.api.urls')),
-   path("arbitros/", include('apps.arbitros.api.urls')),
+   path('jugadores/', include('apps.jugadores.api.urls')),
+   path('arbitros/', include('apps.arbitros.api.urls')),
    path('login/',Login.as_view(),name='login'),
    path('logout/',Logout.as_view(),name="logout"),
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    path('auth/google/',FirebaseRegisterView.as_view(), name='firebase_google_register'),
+   path('login/google/',LoginWhitGoogle.as_view(),name='firebase_login_google')
    #path('refresh-token/',UserToken.as_view(),name='refresh token')
     
 ]
